43c53e771760b5b89e68e9e1231befed0bc8f12f
nobutsune01 <136322737+nobutsune01@users.noreply.github.com>
2023-08-16 20:51:05 +0900

    課題

--- 2-1/src/study/Main.java
+++ 2-1/src/study/Main.java
@@ -30,9 +30,6 @@ public class Main {
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
         
         
-//        	for(int i = 0; i<11; i++) {
-//        		isOdd（i）；  	
-//        	}
         for(int i=0;i<arr.length;i++) {
             isOdd(arr[i]);
            }
